3dbbb8aeba331fe746417a60002676025767f595
dzz007 <andy@dzz007.com>
2018-10-06 17:14:39 -0400

    New things

--- app/src/main/java/com/example/dzz007/swipeoptimizer/Main2Activity.java
+++ app/src/main/java/com/example/dzz007/swipeoptimizer/Main2Activity.java
@@ -2,6 +2,8 @@ package com.example.dzz007.swipeoptimizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
