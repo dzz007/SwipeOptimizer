cf0dd42e83408845951f71461dab3946142df7fb
David Gasperini <dlg4242@live.com>
2018-10-06 14:31:58 -0400

    added start java and xml

---
+++ app/src/main/java/com/example/dzz007/swipeoptimizer/Start.java
@@ -0,0 +1,14 @@
+package com.example.dzz007.swipeoptimizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        setTitle("PLease select a location");
+    }
+}
