3a2aeca6ba3f8cee4949e686d7c9598cdaf6f7b7
dzz007 <andy@dzz007.com>
2018-10-06 13:15:46 -0400

    New things

--- app/src/main/java/com/example/dzz007/swipeoptimizer/MainActivity.java
+++ app/src/main/java/com/example/dzz007/swipeoptimizer/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.dzz007.swipeoptimizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> data = new ArrayList<>();
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data.add("Abc");
+        data.add("Def");
+        ListView lv = (ListView)findViewById(R.id.myView);
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                data.remove(position);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        Button btn = (Button)findViewById(R.id.button5);
+        TextView textTotal = (TextView)findViewById(R.id.textTotal);
+        textTotal.setGravity(Gravity.CENTER);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data.add("WTF?");
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
